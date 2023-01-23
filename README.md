8ad0006a535364866665fa49599d07b3f35fb315
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2023-01-23 17:32:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flag-game
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-qhptgv)
---
+++ countries.js
@@ -0,0 +1,33 @@
+const countries = [
+  {
+    "flag" : "https://raw.githubusercontent.com/nasserkessas/js-flags-quiz/df922c5d5203ab47aa216d3a860b42558ada160b/flags/af.svg",
+    "name" : "Afghanistan"
+  },
+  {
+    "flag" : "https://raw.githubusercontent.com/nasserkessas/js-flags-quiz/df922c5d5203ab47aa216d3a860b42558ada160b/flags/ar.svg",
+    "name" : "Argentina"
+  },
+  {
+    "flag" : "https://raw.githubusercontent.com/nasserkessas/js-flags-quiz/df922c5d5203ab47aa216d3a860b42558ada160b/flags/cm.svg",
+    "name" : "Cameroon"
+  },
+  {
+    "flag" : "https://raw.githubusercontent.com/nasserkessas/js-flags-quiz/df922c5d5203ab47aa216d3a860b42558ada160b/flags/cl.svg",
+    "name" : "Chile"
+  },
+  {
+    "flag" : "https://raw.githubusercontent.com/nasserkessas/js-flags-quiz/df922c5d5203ab47aa216d3a860b42558ada160b/flags/hr.svg",
+    "name" : "Croatia"
+  },
+  {
+    "flag" : "https://raw.githubusercontent.com/nasserkessas/js-flags-quiz/df922c5d5203ab47aa216d3a860b42558ada160b/flags/eg.svg",
+    "name" : "Egypt"
+  },  {
+    "flag" : "https://raw.githubusercontent.com/nasserkessas/js-flags-quiz/df922c5d5203ab47aa216d3a860b42558ada160b/flags/de.svg",
+    "name" : "Germany"
+  },  {
+    "flag" : "https://raw.githubusercontent.com/nasserkessas/js-flags-quiz/df922c5d5203ab47aa216d3a860b42558ada160b/flags/hg.svg",
+    "name" : "Hungary"
+  }
+
+]
